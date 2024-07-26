5dad01e1c1cb8ab15ea35b671bf68fd756187e10
Slothdemon22 <162999948+Slothdemon22@users.noreply.github.com>
2024-07-26 19:38:06 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.json())
 
 app.use(cors({
-    origin: ["https://frontend-ashen-seven-18.vercel.app"], // Allow all origins
+    origin: ["https://frontend-ashen-seven-18.vercel.app","https://admin-panel-gilt-two.vercel.app"], // Allow all origins
     methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"], // Allow these methods
     credentials: true // Allow credentials (cookies, etc.)
 }));
